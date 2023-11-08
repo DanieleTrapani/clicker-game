f064db2c67686aa94c91d860ed86ef49d8974d83
Daniele Trapani <daniele.trapani96@gmail.com>
2023-11-08 12:25:33 +0100

    upgrade component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import earthImg from "./assets/earth.png";
   import spaceImg from "./assets/space.jpeg";
+  import Upgrade from "./lib/Upgrade.svelte";
   let points = 0;
 
   const addPoints = () => {
@@ -8,8 +9,9 @@
   };
 </script>
 
-<main>
-  <h4 class="text-white text-center text-xl mt-4">Score: {points}</h4>
+<main class="flex flex-col">
+  <h4 class="text-white text-center text-xl mt-4">Score: {points} pts</h4>
+  <h6 class="text-white text-center text-xs">0 pts/s</h6>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
   <img
@@ -18,11 +20,16 @@
     class="w-56 m-auto mt-10 hover:scale-110"
     on:click={addPoints}
   />
-  <div />
+  <div
+    class=" flex flex-col gap-4 w-full h-screen bg-gray-900 text-white px-5 py-4 mt-12"
+  >
+    <Upgrade />
+    <Upgrade />
+  </div>
 </main>
 
 <style>
-  h4 {
+  main {
     font-family: "Chakra Petch", sans-serif;
   }
 </style>
---
+++ src/lib/Upgrade.svelte
@@ -0,0 +1,12 @@
+<div
+  class="flex justify-around items-center bg-gray-800 py-2 rounded-md shadow-lg shadow-gray-950"
+>
+  <p>Autoclicker</p>
+  <p>Lv. 10</p>
+  <div
+    class="flex flex-col justify-center items-center bg-[#FFD700] px-3 rounded-xl text-[#0E141D] hover:scale-110"
+  >
+    <p>Buy</p>
+    <p>200pts</p>
+  </div>
+</div>
